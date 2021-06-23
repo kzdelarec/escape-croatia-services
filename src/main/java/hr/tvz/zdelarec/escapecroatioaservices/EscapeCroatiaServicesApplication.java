74f9f1c56b9e253faa7c9482d5ed498009fa61a8
kristijan.zdelarec <kristijan.zdelarecvgmail.com>
2021-06-23 19:14:01 +0200

    Initial commit

---
+++ src/main/java/hr/tvz/zdelarec/escapecroatioaservices/EscapeCroatiaServicesApplication.java
@@ -0,0 +1,11 @@
+package hr.tvz.zdelarec.escapecroatioaservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeCroatiaServicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EscapeCroatiaServicesApplication.class, args);
+    }
+}
