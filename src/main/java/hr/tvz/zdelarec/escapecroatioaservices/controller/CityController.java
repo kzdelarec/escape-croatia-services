93f86917444921742205602ebd5483a5333f0faa
kristijan.zdelarec <kristijan.zdelarecvgmail.com>
2021-07-03 19:52:34 +0200

    Enable SSL

--- src/main/java/hr/tvz/zdelarec/escapecroatioaservices/controller/CityController.java
+++ src/main/java/hr/tvz/zdelarec/escapecroatioaservices/controller/CityController.java
@@ -1,8 +1,9 @@
 package hr.tvz.zdelarec.escapecroatioaservices.controller;
 
 import hr.tvz.zdelarec.escapecroatioaservices.dto.CityDto;
-import hr.tvz.zdelarec.escapecroatioaservices.entity.City;
 import hr.tvz.zdelarec.escapecroatioaservices.service.city.CityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +15,14 @@ import java.util.List;
 @CrossOrigin
 public class CityController {
     public static final String BASE_URL = "/city";
+    private static final Logger LOGGER =  LoggerFactory.getLogger(CityController.class);
 
     @Autowired
     private CityService cityService;
 
     @GetMapping(produces = CityDto.CONTENT_TYPE)
     public List<CityDto> getAll(){
+        LOGGER.info("Dohvat");
         return cityService.getAllCities();
     }
 
