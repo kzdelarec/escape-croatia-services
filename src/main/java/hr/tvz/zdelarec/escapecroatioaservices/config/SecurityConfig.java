55e064acf629196343ed9d4fdf4b75293a99275f
kristijan.zdelarec <kristijan.zdelarecvgmail.com>
2021-08-27 23:56:24 +0200

    Api security issue

--- src/main/java/hr/tvz/zdelarec/escapecroatioaservices/config/SecurityConfig.java
+++ src/main/java/hr/tvz/zdelarec/escapecroatioaservices/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.csrf().ignoringAntMatchers("/login");
+        http.csrf().ignoringAntMatchers("/login", "/api/**");
         http.headers().frameOptions().sameOrigin();
         http
                 .authorizeRequests()
